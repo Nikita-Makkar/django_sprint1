ee16f5998d4466549eba089f64272db5d2dc7a52
Никита Маккар <kish.zk@mail.ru>
2023-07-05 15:17:14 +0300

    last_1

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 
-urlpatterns = [ 
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('pages.urls', namespace='pages')),
-    path('', include('blog.urls', namespace='blog')),]
+    path('', include('blog.urls', namespace='blog')), ]
